e69354bbfbdd262399bfd03e5d728ee395bed478
dancolm95 <dancolm95@gmail.com>
2024-01-08 18:03:24 -0800

    update the tprototype def all(self)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,11 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            return {k: v for k, v in FileStorage.__objects.items() if type(v) == cls}
+
         return FileStorage.__objects
 
     def new(self, obj):
