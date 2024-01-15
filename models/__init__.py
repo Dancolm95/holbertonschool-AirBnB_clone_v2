a7e49828c1894b611a40af25f43c8cf0141807ab
echanjan <erickchanjan300696@gmail.com>
2024-01-14 20:36:43 -0500

    create SQLAlquemy

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
+import os
+from models import storage
 from models.engine.file_storage import FileStorage
 
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage.__class__ = DBStorage
+else:
+    from models.engine.file_storage import FileStorage
+    storage.__class__ = FileStorage
 
-storage = FileStorage()
 storage.reload()
---
+++ models/engine/db_storage.py
@@ -0,0 +1,49 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(os.getenv('HBNB_MYSQL_USER'),
+                                              os.getenv('HBNB_MYSQL_PWD'),
+                                              os.getenv('HBNB_MYSQL_HOST'),
+                                              os.getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
+
+        if os.getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        session = self.__session
+        if cls:
+            objects = session.query(cls).all()
+        else:
+            objects = []
+            for model in Base.metadata.tables.keys():
+                objects.extend(session.query(Base.metadata.tables[model]).all())
+
+        result = {}
+        for obj in objects:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            result[key] = obj
+        return result
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
