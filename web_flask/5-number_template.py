bb7be694eb0d88edab5837f1d51f2759a2e717b8
dancolm95 <dancolm95@gmail.com>
2024-01-23 08:54:49 -0800

    script that starts a web flask application and displays a HTML page

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -57,7 +57,7 @@ def number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """displays an HTML page only if 'n' is an integer"""
-    return render_template("5-number.html", n:n)
+    return render_template("5-number.html", n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
