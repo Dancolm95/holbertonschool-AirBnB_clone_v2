baa4569dc41aa087919e86b3ba1524c836bea49e
echanjan <erickchanjan300696@gmail.com>
2024-01-26 18:45:22 -0500

    fix: line error

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,16 +7,16 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
+def home():
     """Esta ruta muestra un saludo en el home"""
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def main_hbnb():
+def hbnb():
     """Nuestro primer recurso"""
     return "HBNB"
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
