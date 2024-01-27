bed45e872730643eb900decda488e7cccdd42838
echanjan <erickchanjan300696@gmail.com>
2024-01-26 19:00:25 -0500

    fix output

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def home():
     """Esta ruta muestra un saludo en el home"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
